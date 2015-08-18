97a04a96a9579f3cac92459bb62e7b933c7632d2
labaks <las93@bk.ru>
2015-08-18 18:07:23 +0300

    Refactoring

--- app/src/main/java/labaks/dicer/MainActivity.java
+++ app/src/main/java/labaks/dicer/MainActivity.java
@@ -16,51 +16,42 @@ import android.widget.TextView;
 
 public class MainActivity extends ActionBarActivity {
 
-    private final int numberOfDices = 5;
-    private final int diceSides = 6;
-    private TextView[] diceInfo = new TextView[numberOfDices];
+    private static final int NUMBER_OF_DICES = 5;
+    private final int DICE_SIDES = 6;
+    private TextView[] diceInfo = new TextView[NUMBER_OF_DICES];
     private TextView massInfo, combinationInfo, doubleResultOutput;
-    private Dice[] dicesImage = new Dice[numberOfDices];
-    private Bitmap[] croppedDiceImage = new Bitmap[diceSides];
+    private Bitmap[] croppedDiceImage = new Bitmap[DICE_SIDES];
     private final static DisplayMetrics metrics = new DisplayMetrics();
-    private int[] results = new int[diceSides];
+
+    Player firstPlayer;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        firstPlayer = new Player(DICE_SIDES, NUMBER_OF_DICES);
         getWindowManager().getDefaultDisplay().getMetrics(metrics);
 
         initDicesInfo();
 
-        final int diceWidth = (metrics.widthPixels - 20) / numberOfDices;
+        final int diceWidth = (metrics.widthPixels - 20) / NUMBER_OF_DICES;
         cropDiceSides();
-        initDicesImage(croppedDiceImage, diceWidth);
+        initDicesImage(croppedDiceImage, diceWidth, firstPlayer);
         massInfo = (TextView) findViewById(R.id.massInfo);
         combinationInfo = (TextView) findViewById(R.id.combination);
         doubleResultOutput = (TextView) findViewById(R.id.doubleResult);
+
 
         final Button dropTheDices = (Button) findViewById(R.id.dropDice);
         dropTheDices.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                for (int i = 0; i < numberOfDices; i++) {
-                    if (!dicesImage[i].isLeft) {
-                        dicesImage[i].value = dropTheDice();
-                        diceInfo[i].setText(
-                                getString(getBaseContext().getResources().getIdentifier("dice_" + (i + 1), "string", getBaseContext().getPackageName())) +
-                                        ": " + Integer.toString(dicesImage[i].value));
-                        dicesImage[i].diceButton.setImageBitmap(croppedDiceImage[dicesImage[i].value - 1]);
-                    }
-                    checkRepeat(dicesImage[i].value);
-                }
-                outMass(results, diceSides);
-                Rules firstPlayer = new Rules();
-                outputResult(hasCombinations(firstPlayer));
-                resultToNumber(firstPlayer);
-                doubleResultOutput.setText(getString(R.string.numericalResult) + Double.toString(firstPlayer.doubleResult));
-                resetResults();
+                firstPlayer.dropAllowedDices();
+                firstPlayer.hasCombinations();
+                firstPlayer.resultToNumber();
+                printInfo(firstPlayer);
+                firstPlayer.resetValuesCounter();
             }
         });
     }
@@ -87,35 +78,39 @@ public class MainActivity extends ActionBarActivity {
         return super.onOptionsItemSelected(item);
     }
 
-    private static int dropTheDice() {
-        return (int) Math.round(Math.random() * 5 + 1);
-    }
 
     private void initDicesInfo() {
-        for (int i = 0; i < numberOfDices; i++) {
+        for (int i = 0; i < NUMBER_OF_DICES; i++) {
             diceInfo[i] = (TextView) findViewById(getBaseContext().getResources().getIdentifier("diceInfo" + (i + 1), "id", getBaseContext().getPackageName()));
         }
     }
 
-    private void initDicesImage(Bitmap[] background, int diceWidth) {
-        for (int i = 0; i < numberOfDices; i++) {
+    private void initDicesImage(Bitmap[] background, int diceWidth, Player player) {
+        Dice[] dicesImage = new Dice[NUMBER_OF_DICES];
+        for (int i = 0; i < NUMBER_OF_DICES; i++) {
             dicesImage[i] = new Dice();
             dicesImage[i].diceButton = (ImageButton) findViewById(getBaseContext().getResources().getIdentifier("diceImage" + (i + 1), "id", getBaseContext().getPackageName()));
             dicesImage[i].diceButton.setMaxWidth(diceWidth);
             dicesImage[i].diceButton.setImageBitmap(background[i]);
         }
+        player.dices = dicesImage;
     }
 
     private void cropDiceSides() {
         Bitmap bitmapDice = BitmapFactory.decodeResource(this.getResources(), R.drawable.dices_sides);
-        for (int i = 0; i < diceSides; i++) {
+        for (int i = 0; i < DICE_SIDES; i++) {
             croppedDiceImage[i] = Bitmap.createBitmap(bitmapDice, i * 1114, 0, 1114, 1114);
         }
     }
 
     public void onDiceLeft(View view) {
-        for (int i = 0; i < numberOfDices; i++) {
-            if (dicesImage[i].diceButton.getId() == view.getId()) {
+        firstPlayer.dices = onDiceLeftImpl(view.getId());
+    }
+
+    private static Dice[] onDiceLeftImpl(int id) {
+        Dice[] dicesImage = new Dice[NUMBER_OF_DICES];
+        for (int i = 0; i < NUMBER_OF_DICES; i++) {
+            if (dicesImage[i].diceButton.getId() == id) {
                 if (!dicesImage[i].isLeft) {
                     dicesImage[i].diceButton.setBackgroundColor(Color.BLACK);
                     dicesImage[i].switchIsDiceLeft();
@@ -125,88 +120,31 @@ public class MainActivity extends ActionBarActivity {
                 }
             }
         }
+        return dicesImage;
     }
 
-    public void checkRepeat(int diceValue) {
-        results[diceValue - 1]++;
-    }
-
-    public void resetResults() {
-        for (int i = 0; i < diceSides; i++) {
-            results[i] = 0;
+    public void printInfo(Player player) {
+        for (int i = 0; i < NUMBER_OF_DICES; i++) {
+            diceInfo[i].setText(
+                    getString(getBaseContext().getResources().getIdentifier("dice_" + (i + 1), "string", getBaseContext().getPackageName())) +
+                            ": " + Integer.toString(player.dices[i].value));
+            player.dices[i].diceButton.setImageBitmap(croppedDiceImage[player.dices[i].value - 1]);
         }
+        printDroppedValuesCounts(player.droppedValuesCount);
+        outputResult(player);
+        doubleResultOutput.setText(getString(R.string.numericalResult) + Double.toString(player.doubleResult));
     }
 
-    public void outMass(int mass[], int length) {
+    public void printDroppedValuesCounts(int droppedValuesCounts[]) {
         StringBuilder builder = new StringBuilder();
         String separator = " | ";
-        for (int i = 0; i < length; i++) {
-            builder.append(i + 1).append(": ").append(Integer.toString(mass[i])).append(separator);
+        for (int i = 0; i < droppedValuesCounts.length; i++) {
+            builder.append(i + 1).append(": ").append(Integer.toString(droppedValuesCounts[i])).append(separator);
         }
         massInfo.setText(builder.toString());
     }
 
-    public static int maxInMass(int mass[]) {
-        int max = 0;
-        for (int mas : mass) {
-            max = Math.max(max, mas);
-        }
-        return max;
-    }
-
-    public Rules hasCombinations(Rules player) {
-        boolean isMaxOne = maxInMass(results) == 1;
-        if (isMaxOne) {
-            if (results[0] == 0) {
-                player.hasBigStrait = true;
-            } else if (results[5] == 0) {
-                player.hasLittleStrait = true;
-            } else {
-                player.hasNoComb = true;
-            }
-        } else {
-            for (int i = 0; i < diceSides; i++) {
-                if (results[i] == 5) {
-                    player.hasPoker = true;
-                    player.pokerValue = i + 1;
-                } else if (results[i] == 4) {
-                    player.hasFour = true;
-                    player.fourValue = i + 1;
-                } else if (results[i] == 3) {
-                    if (player.hasPair) {
-                        player.hasFullHouse = true;
-                        player.hasPair = false;
-                        player.fullHouseValue[0] = i + 1;
-                        player.fullHouseValue[1] = player.pairValue;
-                        player.pairValue = 0;
-                    } else {
-                        player.hasThree = true;
-                        player.threeValue = i + 1;
-                    }
-                } else if (results[i] == 2) {
-                    if (player.hasPair) {
-                        player.hasTwoPair = true;
-                        player.hasPair = false;
-                        player.twoPairValue[0] = player.pairValue;
-                        player.twoPairValue[1] = i + 1;
-                        player.pairValue = 0;
-                    } else if (player.hasThree) {
-                        player.hasFullHouse = true;
-                        player.hasThree = false;
-                        player.fullHouseValue[0] = player.threeValue;
-                        player.fullHouseValue[1] = i + 1;
-                        player.threeValue = 0;
-                    } else {
-                        player.hasPair = true;
-                        player.pairValue = i + 1;
-                    }
-                }
-            }
-        }
-        return player;
-    }
-
-    public void outputResult(Rules player) {
+    public void outputResult(Player player) {
         StringBuilder builder = new StringBuilder();
         String space = " ";
         if (player.hasPoker) {
@@ -236,26 +174,6 @@ public class MainActivity extends ActionBarActivity {
         } else if (player.hasNoComb) {
             builder.append(getString(R.string.you_have_no_combinations));
             combinationInfo.setText(builder.toString());
-        }
-    }
-
-    public void resultToNumber(Rules player) {
-        if (player.hasPair) {
-            player.doubleResult = 2 + player.pairValue * 0.1;
-        } else if (player.hasTwoPair) {
-            player.doubleResult = 3 + (player.twoPairValue[0] + player.twoPairValue[1]) * 0.1;
-        } else if (player.hasThree) {
-            player.doubleResult = 4 + player.threeValue * 0.1;
-        } else if (player.hasLittleStrait) {
-            player.doubleResult = 5.0;
-        } else if (player.hasBigStrait) {
-            player.doubleResult = 5.5;
-        } else if (player.hasFullHouse) {
-            player.doubleResult = 6 + player.fullHouseValue[0] * 0.1 + player.fullHouseValue[1] * 0.01;
-        } else if (player.hasFour) {
-            player.doubleResult = 7 + player.fourValue * 0.1;
-        } else if (player.hasPoker) {
-            player.doubleResult = 8 + player.pokerValue * 0.1;
         }
     }
 
---
+++ app/src/main/java/labaks/dicer/Player.java
@@ -0,0 +1,143 @@
+package labaks.dicer;
+
+public class Player {
+
+    private final int diceSides,
+            numberOfDices;
+
+    public Dice[] dices;
+
+    boolean hasNoComb = false;
+    boolean hasPair = false;
+    boolean hasTwoPair = false;
+    boolean hasThree = false;
+    boolean hasLittleStrait = false;
+    boolean hasBigStrait = false;
+    boolean hasFullHouse = false;
+    boolean hasFour = false;
+    boolean hasPoker = false;
+
+    int pokerValue = 0;
+    int fourValue = 0;
+    int threeValue = 0;
+    int pairValue = 0;
+
+    int[] fullHouseValue = {0, 0};
+    int[] twoPairValue = {0, 0};
+
+    double doubleResult = 0;
+
+    public int[] droppedValuesCount;
+
+    public Player(int diceSides, int numberOfDices) {
+        this.diceSides = diceSides;
+        this.numberOfDices = numberOfDices;
+        droppedValuesCount = new int[diceSides];
+        dices = new Dice[numberOfDices];
+    }
+
+    private static int generateDiceValue() {
+        return (int) Math.round(Math.random() * 5 + 1);
+    }
+
+    public void dropAllowedDices() {
+        for (int i = 0; i < numberOfDices; i++) {
+            if (dices[i].isLeft) {
+                continue;
+            }
+            dices[i].value = generateDiceValue();
+            increaseValueCounter(dices[i].value);
+
+        }
+    }
+
+    public void resultToNumber() {
+        if (hasPair) {
+            doubleResult = 2 + pairValue * 0.1;
+        } else if (hasTwoPair) {
+            doubleResult = 3 + (twoPairValue[0] + twoPairValue[1]) * 0.1;
+        } else if (hasThree) {
+            doubleResult = 4 + threeValue * 0.1;
+        } else if (hasLittleStrait) {
+            doubleResult = 5.0;
+        } else if (hasBigStrait) {
+            doubleResult = 5.5;
+        } else if (hasFullHouse) {
+            doubleResult = 6 + fullHouseValue[0] * 0.1 + fullHouseValue[1] * 0.01;
+        } else if (hasFour) {
+            doubleResult = 7 + fourValue * 0.1;
+        } else if (hasPoker) {
+            doubleResult = 8 + pokerValue * 0.1;
+        }
+    }
+
+    public static int maxInMass(int mass[]) {
+        int max = 0;
+        for (int mas : mass) {
+            max = Math.max(max, mas);
+        }
+        return max;
+    }
+
+    public void hasCombinations() {
+        boolean isMaxOne = maxInMass(droppedValuesCount) == 1;
+        if (isMaxOne) {
+            if (droppedValuesCount[0] == 0) {
+                hasBigStrait = true;
+            } else if (droppedValuesCount[5] == 0) {
+                hasLittleStrait = true;
+            } else {
+                hasNoComb = true;
+            }
+        } else {
+            for (int i = 0; i < diceSides; i++) {
+                if (droppedValuesCount[i] == 5) {
+                    hasPoker = true;
+                    pokerValue = i + 1;
+                } else if (droppedValuesCount[i] == 4) {
+                    hasFour = true;
+                    fourValue = i + 1;
+                } else if (droppedValuesCount[i] == 3) {
+                    if (hasPair) {
+                        hasFullHouse = true;
+                        hasPair = false;
+                        fullHouseValue[0] = i + 1;
+                        fullHouseValue[1] = pairValue;
+                        pairValue = 0;
+                    } else {
+                        hasThree = true;
+                        threeValue = i + 1;
+                    }
+                } else if (droppedValuesCount[i] == 2) {
+                    if (hasPair) {
+                        hasTwoPair = true;
+                        hasPair = false;
+                        twoPairValue[0] = pairValue;
+                        twoPairValue[1] = i + 1;
+                        pairValue = 0;
+                    } else if (hasThree) {
+                        hasFullHouse = true;
+                        hasThree = false;
+                        fullHouseValue[0] = threeValue;
+                        fullHouseValue[1] = i + 1;
+                        threeValue = 0;
+                    } else {
+                        hasPair = true;
+                        pairValue = i + 1;
+                    }
+                }
+            }
+        }
+    }
+
+    private void increaseValueCounter(int diceValue) {
+        droppedValuesCount[diceValue - 1]++;
+    }
+
+    public void resetValuesCounter() {
+        for (int i = 0; i < diceSides; i++) {
+            droppedValuesCount[i] = 0;
+        }
+    }
+
+}
